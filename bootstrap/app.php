e0949d135e5b129349d76692f839de01470a177c
Koalex70 <mageramovkirill@gmail.com>
2026-05-07 16:24:22 +0300

    Installed the required dependencies via Composer and configured PSR-4 autoloading.

---
+++ bootstrap/app.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$projectRoot = dirname(__DIR__);
+$envPath = $projectRoot . '/.env';
+
+if (is_file($envPath)) {
+    Dotenv::createImmutable($projectRoot)->safeLoad();
+}
+
+date_default_timezone_set($_ENV['APP_TIMEZONE'] ?? 'UTC');
