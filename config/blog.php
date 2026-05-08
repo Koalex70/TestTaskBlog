a5f191b0a493803590c6d434bb2b8f48df733fb6
Koalex70 <mageramovkirill@gmail.com>
2026-05-08 13:13:50 +0300

    Added pagination and blog configuration files

---
+++ config/blog.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'home_latest_posts_per_category' => 3,
+    'related_posts_limit' => 3,
+];
---
+++ config/pagination.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'default_page' => 1,
+    'category_posts_per_page' => 10,
+    'category_posts_per_page_max' => 50,
+    'pagination_window' => 5,
+];
